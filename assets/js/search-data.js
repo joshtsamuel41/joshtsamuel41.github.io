1b7652976413558499a4ea47fd87207fc1e0fc29
joshtsamuel41 <joshsamuel@smu.edu>
2026-03-26 22:06:31 +0000

    Deploying to gh-pages from @ joshtsamuel41/joshtsamuel41.github.io@0790cd845209f69e68801df552bd139b39a40134 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -496,6 +496,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-clt",
+          title: 'CLT',
+          description: "Demonstration of the CLT",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/CLT_2/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
